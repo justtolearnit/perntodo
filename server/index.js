e0591e41971935dfcfd974009726d3a2d5331cad
justtolearnit <justtolearnit@gmail.com>
2021-09-22 07:19:49 +0530

    Successful Get Request

--- server/index.js
+++ server/index.js
@@ -21,6 +21,18 @@ app.post("/todos", async (req, res) => {
     }
 })
 
+//get all todo
+
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodo = await pool.query("SELECT * FROM todo");
+        res.json(allTodo.rows)
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+
 app.listen(5000, () => {
     console.log("Server started at port 5000");
 })
