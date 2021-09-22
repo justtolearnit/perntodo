03582a0aa9e3bae2483ac460b309fba340f181ee
justtolearnit <justtolearnit@gmail.com>
2021-09-22 10:08:38 +0530

    Posting on Sever is completed again after -Faisal's self evaluation

--- client/src/InputTodo.js
+++ client/src/InputTodo.js
@@ -3,14 +3,34 @@ import { useState } from 'react';
 
 export default function InputTodo() {
 
-    const [description, setDescription] = useState()
+    const [description, setDescription] = useState("")
+
+    const submitHandler = async e =>{
+        e.preventDefault();
+        try {
+            const body = {description};
+            const response = await fetch(
+                'http://localhost:5000/todos',
+                {
+                    method:"POST",
+                    headers:{"Content-Type":"application/json"},
+                    body:JSON.stringify(body)
+                });
+        console.log(response)
+            
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
 
     return (
         <Fragment>
+            <form onSubmit={submitHandler}>
             <input type='text' value={description}
                 onChange={e => setDescription(e.target.value)}
             ></input>
-            <button>Add</button>
+            <button type='submit'>Add</button>
+            </form>
         </Fragment>
     )
 }
