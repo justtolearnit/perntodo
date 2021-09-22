5d7a33e42e4c7cb16c55fc351650d8d2b0448028
justtolearnit <justtolearnit@gmail.com>
2021-09-22 07:18:58 +0530

    Successfull Post Request

--- server/index.js
+++ server/index.js
@@ -4,16 +4,23 @@ const app = express();
 const pool = require("./db")
 
 app.use(cors());
-app.use(express.json())//"To request body"
+app.use(express.json())//"To request req.body"
 
 //Routes
 //create a todo
-//get all todo
-//get a todo
-//update a todo
-//delete a todo
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *", [description]
+        );
+        res.json(newTodo.rows[0]);
 
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("Server started at port 5000");
 })
